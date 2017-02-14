8411401a61d2cfb3a1a69e15749e1732bf92fd52
Mark Laffoon <laffoon@san.rr.com>
2017-02-14 12:04:44 -0800

    tweaks (#11)
    change exam processor to be a sink
    rename message queue
    deal with payload being a String not a marshalled object

---
+++ exam-processor/src/test/java/org/rdw/ingest/processor/ExamProcessorConfigurationTest.java
@@ -0,0 +1,27 @@
+package org.rdw.ingest.processor;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.GenericMessage;
+import rdw.messaging.RdwMessageHeaderAccessor;
+
+public class ExamProcessorConfigurationTest {
+
+    private ExamProcessorConfiguration processor;
+
+    @Before
+    public void createProcessor() {
+        processor = new ExamProcessorConfiguration();
+    }
+
+    @Test
+    public void itShouldProcessMessageWithXmlContent() {
+        final RdwMessageHeaderAccessor accessor = RdwMessageHeaderAccessor.wrap(null);
+        accessor.setContent("exams");
+        accessor.setContentType(MediaType.APPLICATION_XML);
+        final Message message = new GenericMessage("<TDSReport/>", accessor.getMessageHeaders());
+        processor.process(message);
+    }
+}
