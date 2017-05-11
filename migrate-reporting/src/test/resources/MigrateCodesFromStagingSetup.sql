1678f309f56a6cc30558a8939f3d5c51003219da
Alla Gorina <agorina@fairwaytech.com>
2017-05-11 15:55:43 -0700

    First try at adding codes migration.

---
+++ migrate-reporting/src/test/resources/MigrateCodesFromStagingSetup.sql
@@ -0,0 +1,3 @@
+INSERT INTO `staging-test`.staging_completeness (id, name) VALUES (-99, 'test1');
+INSERT INTO `staging-test`.staging_completeness (id, name) VALUES (-88, 'test2');
+INSERT INTO `staging-test`.staging_completeness (id, name) VALUES (-55, 'test55');
