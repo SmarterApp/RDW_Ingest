3f3d388ac6bafdf79c2bf851ac67e089b66fc3eb
Rob Cordeau <rcordeau@fairwaytech.com>
2017-05-15 10:06:54 -0700

    renaming all usages of schemas named *-test to *_test

--- migrate-reporting/src/test/resources/MigrateCodesPreloadReportingSetup.sql
+++ migrate-reporting/src/test/resources/MigrateCodesPreloadReportingSetup.sql
@@ -1,15 +1,15 @@
-INSERT INTO `reporting-test`.grade (id, code, name) VALUES (-99,'AA','before test');
-INSERT INTO `reporting-test`.completeness (id, name) VALUES (-99, 'before');
-INSERT INTO `reporting-test`.administration_condition VALUES (-99,'before test');
-INSERT INTO `reporting-test`.ethnicity VALUES (-99,'before test');
-INSERT INTO `reporting-test`.gender VALUES (-99,'before test');
-INSERT INTO `reporting-test`.accommodation VALUES(-99, 'before test');
-INSERT INTO `reporting-test`.claim VALUES (-99 ,2, 'claim', 'before test','before test');
-INSERT INTO `reporting-test`.depth_of_knowledge  VALUES (-99, 1, 1,'before test', 'before');
-INSERT INTO `reporting-test`.math_practice  VALUES (-99,'before test');
-INSERT INTO `reporting-test`.item_trait_score VALUES (-99,'before test');
-INSERT INTO `reporting-test`.target  VALUES (-99,-99,'10-1','before test');
-INSERT INTO `reporting-test`.item_difficulty_cuts  VALUES (-99, 1, 2, -99,-2.2,-3.3);
+INSERT INTO `reporting_test`.grade (id, code, name) VALUES (-99,'AA','before test');
+INSERT INTO `reporting_test`.completeness (id, name) VALUES (-99, 'before');
+INSERT INTO `reporting_test`.administration_condition VALUES (-99,'before test');
+INSERT INTO `reporting_test`.ethnicity VALUES (-99,'before test');
+INSERT INTO `reporting_test`.gender VALUES (-99,'before test');
+INSERT INTO `reporting_test`.accommodation VALUES(-99, 'before test');
+INSERT INTO `reporting_test`.claim VALUES (-99 ,2, 'claim', 'before test','before test');
+INSERT INTO `reporting_test`.depth_of_knowledge  VALUES (-99, 1, 1,'before test', 'before');
+INSERT INTO `reporting_test`.math_practice  VALUES (-99,'before test');
+INSERT INTO `reporting_test`.item_trait_score VALUES (-99,'before test');
+INSERT INTO `reporting_test`.target  VALUES (-99,-99,'10-1','before test');
+INSERT INTO `reporting_test`.item_difficulty_cuts  VALUES (-99, 1, 2, -99,-2.2,-3.3);
 
 
 
