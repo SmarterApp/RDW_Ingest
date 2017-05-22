dbbdbbf64053930536ffb3c5281a0044f5ac5e90
Alla Gorina <agorina@fairwaytech.com>
2017-05-22 13:37:24 -0700

    Migrate exam step.

--- migrate-reporting/src/test/resources/MigrateEntitiesFromStagingForDeleteSetup.sql
+++ migrate-reporting/src/test/resources/MigrateEntitiesFromStagingForDeleteSetup.sql
@@ -9,10 +9,20 @@ DELETE FROM staging_test.staging_asmt_score WHERE asmt_id = -99;
 DELETE FROM staging_test.staging_item WHERE asmt_id = -99;
 
 -- ------------------------------------------ Student and Groups  ------------------------------------------------------------------------------------------------
-
 UPDATE staging_test.staging_student SET deleted = 1 WHERE id = -89;
 UPDATE staging_test.staging_student_group SET deleted = 1 WHERE id = -91;
 DELETE from staging_test.staging_student_ethnicity where student_id = -91;
 DELETE from staging_test.staging_student_group_membership where student_group_id = -91;
 DELETE from staging_test.staging_user_student_group where student_group_id = -91;
 
+-- ------------------------------------------ IAB Exams ---------------------------------------------------------------------------------------------
+UPDATE staging_test.staging_iab_exam SET deleted = 1 WHERE id = -88;
+DELETE from staging_test.staging_iab_exam_student WHERE id = -18;
+DELETE from staging_test.staging_iab_exam_available_accommodation  WHERE iab_exam_id = -88;
+DELETE from staging_test.staging_iab_exam_item  WHERE iab_exam_id = -88;
+
+ ------------------------------------------  Exams ---------------------------------------------------------------------------------------------
+UPDATE staging_test.staging_exam SET deleted = 1 WHERE id = -88;
+DELETE from staging_test.staging_exam_student WHERE id = -18;
+DELETE from staging_test.staging_exam_available_accommodation  WHERE exam_id = -88;
+DELETE from staging_test.staging_exam_item  WHERE exam_id = -88;
--- migrate-reporting/src/test/resources/MigrateEntitiesFromStagingTearDown.sql
+++ migrate-reporting/src/test/resources/MigrateEntitiesFromStagingTearDown.sql
@@ -1,26 +1,46 @@
 -- CLEAN UP staging
+-- ------------------------------------------ IAB Exams ---------------------------------------------------------------------------------------------
+DELETE FROM staging_test.staging_iab_exam_available_accommodation where iab_exam_id in (-88, -87, -86, -85, -84);
+DELETE FROM staging_test.staging_iab_exam_item where iab_exam_id in (-88, -87, -86, -85, -84);
+DELETE FROM staging_test.staging_iab_exam where id in (-88, -87, -86, -85, -84);
+DELETE FROM staging_test.staging_iab_exam_student where id in (-18, -17, -16);
+
+DELETE FROM staging_test.staging_exam_available_accommodation where exam_id in (-88, -87, -86, -85, -84);
+DELETE FROM staging_test.staging_exam_item where exam_id in (-88, -87, -86, -85, -84);
+DELETE FROM staging_test.staging_exam_claim_score where exam_id in (-88, -87, -86, -85, -84);
+DELETE FROM staging_test.staging_exam where id in (-88, -87, -86, -85, -84);
+DELETE FROM staging_test.staging_exam_student where id in (-18, -17, -16);
 -- ------------------------------------------ School/Districts --------------------------------------------------------------------------------------------------
 DElETE FROM staging_test.staging_school where id in ( -99, -98, -1);
 DElETE FROM staging_test.staging_district where id in ( -99, -98, -1);
 
 -- ------------------------------------------ Asmt ---------------------------------------------------------------------------------------------------------
-DElETE FROM staging_test.staging_item where id in ( -990, -991, -992, -993, -980, -981, -982, -983, -984);
+DElETE FROM staging_test.staging_item where asmt_id in ( -11, -99, -98);
 DElETE FROM staging_test.staging_asmt_score where asmt_id in ( -99, -98);
-DElETE FROM staging_test.staging_asmt where id in ( -99, -98);
+DElETE FROM staging_test.staging_asmt where id in (-11, -99, -98);
 
 -- ------------------------------------------ Student and Groups  ------------------------------------------------------------------------------------------------
 DELETE FROM staging_test.staging_student_group_membership where student_group_id in ( -91, -8, -7);
 DELETE FROM staging_test.staging_user_student_group where student_group_id in ( -91, -8, -7);
-DELETE FROM staging_test.staging_student where id in ( -89, -88, -87, -86);
+DELETE FROM staging_test.staging_student where id in (-11,  -89, -88, -87, -86);
 DELETE FROM staging_test.staging_student_ethnicity where student_id in ( -89, -88, -87, -86);
 DELETE FROM staging_test.staging_student_group where id in ( -91, -8, -7);
 
 
 -- CLEAN UP reporting
+-- ------------------------------------------ IAB Exams ---------------------------------------------------------------------------------------------
+DELETE FROM reporting_test.iab_exam_available_accommodation where iab_exam_id in (-88, -87, -86, -85, -84);
+DELETE FROM reporting_test.iab_exam_item where iab_exam_id in (-88, -87, -86, -85, -84);
+DELETE FROM reporting_test.iab_exam where id in (-88, -87, -86, -85, -84);
+
+DELETE FROM reporting_test.exam_available_accommodation where exam_id in (-88, -87, -86, -85, -84);
+DELETE FROM reporting_test.exam_item where exam_id in (-88, -87, -86, -85, -84);
+DELETE FROM reporting_test.exam where id in (-88, -87, -86, -85, -84);
+
 -- ------------------------------------------ Student and Groups  ------------------------------------------------------------------------------------------------
 DELETE FROM reporting_test.student_group_membership where student_group_id in ( -91, -8, -7);
 DELETE FROM reporting_test.user_student_group where student_group_id in ( -91, -8, -7);
-DELETE FROM reporting_test.student where id in ( -89, -88, -87, -86);
+DELETE FROM reporting_test.student where id in ( -11, -89, -88, -87, -86);
 DELETE FROM reporting_test.student_ethnicity where student_id in ( -89, -88, -87, -86);
 DELETE FROM reporting_test.student_group where id in ( -91, -8, -7);
 
@@ -29,9 +49,9 @@ DElETE FROM reporting_test.school where id in ( -99, -98, -1);
 DElETE FROM reporting_test.district where id in ( -99, -98, -1);
 
 -- ------------------------------------------ Asmt ---------------------------------------------------------------------------------------------------------
-DElETE FROM reporting_test.item where id in ( -990, -991, -992, -993, -980, -981, -982, -983, -984, -9);
+DElETE FROM reporting_test.item where asmt_id in ( -11, -99, -98);
 DElETE FROM reporting_test.asmt_score where asmt_id in ( -99, -98);
-DElETE FROM reporting_test.asmt where id in ( -99, -98);
+DElETE FROM reporting_test.asmt where id in ( -11, -99, -98);
 
 DELETE FROM reporting_test.migrate;
 
