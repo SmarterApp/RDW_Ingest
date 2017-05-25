291097129000b78b349cb9e20755c2765ab41063
malaffoon <laffoon@san.rr.com>
2017-05-25 16:26:43 -0700

    initial checkin

--- migrate-reporting/src/test/resources/MigrateImportFromWarehouseSetup.sql
+++ migrate-reporting/src/test/resources/MigrateImportFromWarehouseSetup.sql
@@ -6,8 +6,8 @@ WHERE id = 1;
 -- Completed migration import id's in reporting
 -- First migrate completed does not exist in warehouse, just for testing
 INSERT INTO reporting_test.migrate (id, job_id, status, first_import_id, last_import_id) VALUES
-  (-1, 1, 20, -1020, -1011),
-  (-2, 2, 20, -1010, -1001);
+  (-10, 1, 20, -1020, -1011),
+  (-9, 2, 20, -1010, -1001);
 
 -- Test import records in warehouse
 -- todo: what is batch and message for?
