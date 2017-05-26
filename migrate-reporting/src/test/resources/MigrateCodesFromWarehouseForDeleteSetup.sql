24031b1cf696937bd1616deaa9dd2f54fe06d908
Chris Dietz <cdietz@fairwaytech.com>
2017-05-26 13:48:11 -0700

    added support to migrate from warehouse scripts for language and accommodation and config copy codes step

--- migrate-reporting/src/test/resources/MigrateCodesFromWarehouseForDeleteSetup.sql
+++ migrate-reporting/src/test/resources/MigrateCodesFromWarehouseForDeleteSetup.sql
@@ -12,8 +12,7 @@ INSERT INTO warehouse_test.claim VALUES (-98, 2, '2-W', 'Write Effectively', 'Wr
 INSERT INTO warehouse_test.depth_of_knowledge VALUES (-98, 2, 1, 'Basic Skills and Concepts', 'anything');
 INSERT INTO warehouse_test.math_practice VALUES (-98, 'Reason abstractly and quantitatively');
 INSERT INTO warehouse_test.item_trait_score VALUES (-98, 'Evidence/Elaboration-test');
-INSERT INTO warehouse_test.target VALUES (-98, -98, '10-12', 'WORD MEANINGS:..)');
+INSERT INTO warehouse_test.target VALUES (-98, 'natural-98', -98, '10-12', 'WORD MEANINGS:..)');
 INSERT INTO warehouse_test.item_difficulty_cuts (id, asmt_type_id, subject_id, grade_id, moderate_low_end, difficult_low_end) VALUES (-98, 1, 2, -98, -1.51022, 0.14288);
-
-
-
+INSERT INTO warehouse_test.language VALUES (-98,'gua');
+INSERT INTO warehouse_test.accommodation_translation VALUES (-98, -98, 'Adios');
