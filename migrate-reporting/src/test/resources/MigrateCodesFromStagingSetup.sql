63493fa28cdff6f94891bb2b1e56cf5f4844b579
Alla Gorina <agorina@fairwaytech.com>
2017-05-30 13:29:02 -0700

    Renamed 'name' to 'code'.

--- migrate-reporting/src/test/resources/MigrateCodesFromStagingSetup.sql
+++ migrate-reporting/src/test/resources/MigrateCodesFromStagingSetup.sql
@@ -1,5 +1,5 @@
 INSERT INTO staging_test.staging_grade (id, code, name) VALUES (-99,'99','K-garten'),(-98 ,'98','1st-grade');
-INSERT INTO staging_test.staging_completeness (id, name) VALUES (-99, 'test1'), (-98, 'test55');
+INSERT INTO staging_test.staging_completeness (id, code) VALUES (-99, 'test1'), (-98, 'test55');
 INSERT INTO staging_test.staging_administration_condition VALUES (-99,'IN-test'),(-98,'NS-test'),(-97,'SD-test'),(-96,'Valid-test');
 INSERT INTO staging_test.staging_ethnicity VALUES (-99,'AmericanIndianOrAlaskaNative-test'),(-98,'Asian-test');
 INSERT INTO staging_test.staging_gender VALUES (-99,'Female-test'),(-98,'Male-test');
