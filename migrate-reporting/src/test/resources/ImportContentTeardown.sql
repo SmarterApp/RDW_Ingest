700ec35ac4bdaf832bba4557f55366f04d489e40
Chris Dietz <cdietz@fairwaytech.com>
2017-06-06 09:28:35 -0700

    refactor sql test file names and consolidate

--- migrate-reporting/src/test/resources/ImportContentTeardown.sql
+++
@@ -1,7 +0,0 @@
--- todo: remove this from initial dml
-INSERT INTO warehouse_test.import (id, status, content, contentType, digest) VALUES
-  (1, 1, 3, 'initial load', 'initial load');
-
--- Delete imports
-DELETE FROM warehouse_test.import
-WHERE id IN (-1, -2, -20, -89, -88, -87, -86, -79, -90, -99, -5000, 2000);
