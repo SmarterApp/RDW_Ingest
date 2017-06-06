9aaf2f00c24a4417db9d78261ae1ae48aa259b71
Chris Dietz <cdietz@fairwaytech.com>
2017-06-06 00:49:04 -0700

    test for stage codes step

---
+++ migrate-reporting/src/test/resources/WarehouseStagingCodesTruncate.sql
@@ -0,0 +1,20 @@
+/*
+ *  warehouse to staging step tests that copy codes need to truncate staging to clear out
+ *  initial dml codes that get copied.
+ */
+TRUNCATE staging_test.staging_completeness;
+TRUNCATE staging_test.staging_administration_condition;
+TRUNCATE staging_test.staging_ethnicity;
+TRUNCATE staging_test.staging_gender;
+TRUNCATE staging_test.staging_accommodation_translation;
+TRUNCATE staging_test.staging_accommodation;
+TRUNCATE staging_test.staging_depth_of_knowledge;
+TRUNCATE staging_test.staging_math_practice;
+TRUNCATE staging_test.staging_item_trait_score;
+TRUNCATE staging_test.staging_target;
+TRUNCATE staging_test.staging_common_core_standard;
+TRUNCATE staging_test.staging_item_difficulty_cuts;
+TRUNCATE staging_test.staging_claim;
+TRUNCATE staging_test.staging_grade;
+TRUNCATE staging_test.staging_language;
+
