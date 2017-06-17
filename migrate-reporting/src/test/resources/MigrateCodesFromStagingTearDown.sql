112beb88c3c7e8bbaa5c0b1bbb4f1fc699fc204f
Rob Cordeau <rcordeau@mac.com>
2017-06-16 17:59:11 -0700

    Feature/dwr 296 translations denormalized (#243)
    * accommodation_translation to translations working
    
    * updated names
    
    * updated schema dependency

--- migrate-reporting/src/test/resources/MigrateCodesFromStagingTearDown.sql
+++ migrate-reporting/src/test/resources/MigrateCodesFromStagingTearDown.sql
@@ -2,7 +2,7 @@ DELETE FROM staging_test.staging_completeness WHERE id IN (-99,-98);
 DELETE FROM staging_test.staging_administration_condition WHERE id IN (-99,-98, -97, -96);
 DELETE FROM staging_test.staging_ethnicity WHERE id IN (-99,-98);
 DELETE FROM staging_test.staging_gender WHERE id IN (-99,-98);
-DELETE FROM staging_test.staging_accommodation_translation WHERE accommodation_id IN (-99, -98) AND language_id in (-99, -98);
+DELETE FROM staging_test.staging_translation WHERE label_code IN ('code1-test', 'code2-test') AND language_code in ('lan', 'gua') AND namespace = 'backend';
 DELETE FROM staging_test.staging_accommodation WHERE id IN (-99,-98);
 DELETE FROM staging_test.staging_depth_of_knowledge  WHERE id IN (-99,-98);
 DELETE FROM staging_test.staging_math_practice  WHERE practice IN (-99,-98);
@@ -11,7 +11,6 @@ DELETE FROM staging_test.staging_target WHERE id IN (-99,-98);
 DELETE FROM staging_test.staging_common_core_standard WHERE id IN (-99,-98);
 DELETE FROM staging_test.staging_claim WHERE id IN (-99,-98);
 DELETE FROM staging_test.staging_grade WHERE id IN (-99,-98);
-DELETE FROM staging_test.staging_language WHERE id IN (-99, -98);
 
 
 
@@ -19,7 +18,7 @@ DELETE FROM reporting_test.completeness WHERE id IN (-99,-98);
 DELETE FROM reporting_test.administration_condition WHERE id IN (-99,-98, -97, -96);
 DELETE FROM reporting_test.ethnicity WHERE id IN (-99,-98);
 DELETE FROM reporting_test.gender  WHERE id IN (-99,-98);
-DELETE FROM reporting_test.accommodation_translation WHERE accommodation_id IN (-99, -98) AND language_id in (-99, -98);
+DELETE FROM reporting_test.translation WHERE label_code IN ('code1-test', 'code2-test', 'before_test_code') AND language_code in ('lan', 'gua', 'bef') AND namespace = 'backend';
 DELETE FROM reporting_test.accommodation WHERE id IN (-99,-98);
 DELETE FROM reporting_test.depth_of_knowledge  WHERE id IN (-99,-98);
 DELETE FROM reporting_test.math_practice  WHERE practice IN (-99,-98);
@@ -28,7 +27,6 @@ DELETE FROM reporting_test.target  WHERE id IN (-99,-98);
 DELETE FROM reporting_test.common_core_standard  WHERE id IN (-99,-98);
 DELETE FROM reporting_test.grade WHERE id IN (-99,-98);
 DELETE FROM reporting_test.claim WHERE id IN (-99,-98);
-DELETE FROM reporting_test.language WHERE id IN (-99, -98);
 
 
 DELETE FROM reporting_test.migrate;
--- migrate-reporting/src/test/resources/ReportingCodesPreload.sql
+++ migrate-reporting/src/test/resources/ReportingCodesPreload.sql
@@ -10,6 +10,5 @@ INSERT INTO reporting_test.math_practice  VALUES (-99,'before test');
 INSERT INTO reporting_test.item_trait_score VALUES (-99,'before test');
 INSERT INTO reporting_test.target  VALUES (-99,-99,'10-1','before test');
 INSERT INTO reporting_test.common_core_standard  VALUES (-99, 'naturalId-99', 1, 'before test');
-INSERT INTO reporting_test.language VALUES (-99,'bef');
-INSERT INTO reporting_test.accommodation_translation VALUES (-99, -99,'before test');
+INSERT INTO reporting_test.translation (namespace, label_code, language_code, label) VALUES ('backend', 'code1-test', 'lan', 'before test');
 
