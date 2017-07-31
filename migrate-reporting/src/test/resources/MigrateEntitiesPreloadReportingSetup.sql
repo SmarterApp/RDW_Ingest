045249d83c6a1accbbd9ffd32052b61579e7c3ad
Alla Gorina <agorina@fairwaytech.com>
2017-07-31 15:08:07 -0700

    DWR-637 Change migreate to be based on the timestamp range

--- migrate-reporting/src/test/resources/MigrateEntitiesPreloadReportingSetup.sql
+++ migrate-reporting/src/test/resources/MigrateEntitiesPreloadReportingSetup.sql
@@ -3,14 +3,14 @@ INSERT INTO reporting_test.district (id, name, natural_id) VALUES
   (-1, 'Before Test -1', 'natural_id-1'),
   (-99, 'Before Test -99', 'natural_id-99');
 
-INSERT INTO reporting_test.school (id, district_id, name, natural_id, import_id) VALUES
-  (-1, -1, 'Before Test -1', 'natural_id-1', -5000),
-  (-99, -99, 'Before Test -99', 'natural_id-99', -1);
+INSERT INTO reporting_test.school (id, district_id, name, natural_id, import_id, updated) VALUES
+  (-1, -1, 'Before Test -1', 'natural_id-1', -5000, '2017-05-18 20:06:34.966000'),
+  (-99, -99, 'Before Test -99', 'natural_id-99', -1, '2017-07-18 20:14:34.000000');
 
 -- ------------------------------------------ Asmt ---------------------------------------------------------------------------------------------------------
-INSERT INTO reporting_test.asmt (id, natural_id, grade_id, grade_code, type_id, subject_id, school_year, name, label, version, import_id, cut_point_1, cut_point_2, cut_point_3, min_score, max_score) VALUES
-   (-11, '(SBAC)SBAC-IAB-ASMT TEST-11', -98, '98', 2, 1, 2016, 'SBAC-IAB-FIXED-G4M-OA-MATH-4', 'test', '9835', -1, 2442, 2502, 2582, 2201, 2701),
-   (-99, '(SBAC)SBAC-IAB-ASMT TEST-99', -99, '99', 2, 1, 2016, 'SBAC-IAB-FIXED-G4M-OA-MATH-4', 'test', '9835', -1, 2442, 2502, 2582, 2201, 2701);
+INSERT INTO reporting_test.asmt (id, natural_id, grade_id, grade_code, type_id, subject_id, school_year, name, label, version, import_id, cut_point_1, cut_point_2, cut_point_3, min_score, max_score, updated) VALUES
+   (-11, '(SBAC)SBAC-IAB-ASMT TEST-11', -98, '98', 2, 1, 2016, 'SBAC-IAB-FIXED-G4M-OA-MATH-4', 'test', '9835', -1, 2442, 2502, 2582, 2201, 2701, '2017-07-18 20:14:34.000000'),
+   (-99, '(SBAC)SBAC-IAB-ASMT TEST-99', -99, '99', 2, 1, 2016, 'SBAC-IAB-FIXED-G4M-OA-MATH-4', 'test', '9835', -1, 2442, 2502, 2582, 2201, 2701, '2017-07-18 20:14:34.000000');
 
 INSERT INTO reporting_test.asmt_score (asmt_id, cut_point_1, cut_point_2, cut_point_3, min_score, max_score) VALUES
   (-99, 2442, 2502, 2582, 2201, 2701);
@@ -36,15 +36,15 @@ INSERT INTO reporting_test.item_common_core_standard(item_id, common_core_standa
 
 -- ------------------------------------------ Student and Groups  ------------------------------------------------------------------------------------------------
 INSERT INTO reporting_test.student (id, ssid, last_or_surname, first_name, middle_name, gender_id, gender_code, first_entry_into_us_school_at, lep_entry_at,
-        lep_exit_at, birthday, import_id) VALUES
-   (-11, '11', 'TestName', 'FirstName2', 'MiddleName2', -98, 'before_test', '2012-08-14', '2012-11-13', null, '2000-01-01', -5000),
-   (-89, '89', 'TestName', 'FirstName2', 'MiddleName2', -98, 'before_test', '2012-08-14', '2012-11-13', null, '2000-01-01', -5000);
+        lep_exit_at, birthday, import_id, updated) VALUES
+   (-11, '11', 'TestName', 'FirstName2', 'MiddleName2', -98, 'before_test', '2012-08-14', '2012-11-13', null, '2000-01-01', -5000, '2017-05-18 20:06:34.966000'),
+   (-89, '89', 'TestName', 'FirstName2', 'MiddleName2', -98, 'before_test', '2012-08-14', '2012-11-13', null, '2000-01-01', -5000, '2017-05-18 20:06:34.966000');
 
 INSERT INTO reporting_test.student_ethnicity(student_id, ethnicity_id, ethnicity_code) values
     (-89,  -99, 'before test');
 
-INSERT INTO reporting_test.student_group (id, creator, school_id, school_year, name, subject_id, import_id) VALUES
-   (-91, 'TestName', -1, 2017, 'Test Student Group 9', null, -5000);
+INSERT INTO reporting_test.student_group (id, creator, school_id, school_year, name, subject_id, import_id, created, updated) VALUES
+   (-91, 'TestName', -1, 2017, 'Test Student Group 9', null, -5000, '2017-05-18 19:05:33.967000', '2017-05-18 20:06:34.966000');
 
 INSERT INTO reporting_test.student_group_membership (student_group_id, student_id) VALUES
    (-91, -89);
@@ -62,9 +62,10 @@ INSERT INTO  reporting_test.exam (id, type_id, school_year, asmt_id, asmt_versio
                                 claim1_scale_score, claim1_scale_score_std_err,claim1_category,
                                 claim2_scale_score, claim2_scale_score_std_err,claim2_category,
                                 claim3_scale_score, claim3_scale_score_std_err,claim3_category,
-                                claim4_scale_score, claim4_scale_score_std_err,claim4_category) VALUES
+                                claim4_scale_score, claim4_scale_score_std_err,claim4_category,
+                                updated) VALUES
 (-88, 1,  2016, -99,  null, 1, -99, -99, 'before test', 'test', 1, 2145, 0.17, '2016-08-14', -88, -98, -89, -1, 1, 1, 0, 0, 1,
-  'test', 'test', 'eng', null, 'before', '98', 2000, 0.11, 1, 2100, 0.12, 2, 2500, 0.13, 3, 3000, .15, 4);
+  'test', 'test', 'eng', null, 'before', '98', 2000, 0.11, 1, 2100, 0.12, 2, 2500, 0.13, 3, 3000, .15, 4, '2017-07-18 19:06:07.966000');
 
 INSERT INTO reporting_test.exam_available_accommodation (exam_id, accommodation_id) VALUES
     (-88, -98);
