7e33148b96fd4c6830501a5de0f72e3871d9693c
Alla Gorina <agorina@fairwaytech.com>
2017-08-01 19:58:48 -0700

    DWR-689 Merger all reporting schemas - Part 1

--- migrate-reporting/src/test/resources/WarehouseStagingCodesTruncate.sql
+++ migrate-reporting/src/test/resources/WarehouseStagingCodesTruncate.sql
@@ -2,17 +2,17 @@
  *  warehouse to staging step tests that copy codes need to truncate staging to clear out
  *  initial dml codes that get copied.
  */
-TRUNCATE staging_test.staging_completeness;
-TRUNCATE staging_test.staging_administration_condition;
-TRUNCATE staging_test.staging_ethnicity;
-TRUNCATE staging_test.staging_gender;
-TRUNCATE staging_test.staging_translation;
-TRUNCATE staging_test.staging_accommodation;
-TRUNCATE staging_test.staging_depth_of_knowledge;
-TRUNCATE staging_test.staging_math_practice;
-TRUNCATE staging_test.staging_item_trait_score;
-TRUNCATE staging_test.staging_target;
-TRUNCATE staging_test.staging_common_core_standard;
-TRUNCATE staging_test.staging_claim;
-TRUNCATE staging_test.staging_grade;
+TRUNCATE reporting_test.staging_completeness;
+TRUNCATE reporting_test.staging_administration_condition;
+TRUNCATE reporting_test.staging_ethnicity;
+TRUNCATE reporting_test.staging_gender;
+TRUNCATE reporting_test.staging_translation;
+TRUNCATE reporting_test.staging_accommodation;
+TRUNCATE reporting_test.staging_depth_of_knowledge;
+TRUNCATE reporting_test.staging_math_practice;
+TRUNCATE reporting_test.staging_item_trait_score;
+TRUNCATE reporting_test.staging_target;
+TRUNCATE reporting_test.staging_common_core_standard;
+TRUNCATE reporting_test.staging_claim;
+TRUNCATE reporting_test.staging_grade;
 
