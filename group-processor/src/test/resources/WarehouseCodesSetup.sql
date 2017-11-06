ff0548875c46147c04b56ea624f7dedb59c9c950
Alla Gorina <agorina@fairwaytech.com>
2017-11-06 13:41:15 -0800

    DWR-1132 Ingest: support accommodations updates, remove language restrictions

--- group-processor/src/test/resources/WarehouseCodesSetup.sql
+++ group-processor/src/test/resources/WarehouseCodesSetup.sql
@@ -7,7 +7,7 @@ INSERT INTO completeness (id, code) VALUES (-99, 'test1'), (-98, 'test55');
 INSERT INTO administration_condition VALUES (-99,'IN-test'),(-98,'NS-test'),(-97,'SD-test'),(-96,'Valid-test');
 INSERT INTO ethnicity VALUES (-99,'AmericanIndianOrAlaskaNative-test'),(-98,'Asian-test');
 INSERT INTO gender VALUES (-99,'Female-test'),(-98,'Male-test');
-INSERT INTO accommodation VALUES(-99, 'code1-test'),( -98 ,'code2-test');
+INSERT INTO accommodation (id, code) VALUES(-99, 'code1-test'),( -98 ,'code2-test');
 INSERT INTO claim VALUES (-99 ,2,'1-IT','Read Analytically: Informational Text','Read Analytically: Informational Text ...'),( -98,2,'2-W','Write Effectively','Write Effectively - ..');
 INSERT INTO depth_of_knowledge  VALUES (-99,1,1,'Recall and Reproduction','something'),(-98,2,1,'Basic Skills and Concepts','anything');
 INSERT INTO math_practice  VALUES (-99,'Make sense of problems and persevere in solving them', '-9'),(-98,'Reason abstractly and quantitatively', '-8');
@@ -15,5 +15,4 @@ INSERT INTO item_trait_score VALUES (-99,'Conventions-test'),(-98,'Evidence/Elab
 INSERT INTO target VALUES (-99, 'natural-99', -99,'10-1','WORD MEANINGS: Determine intended meanings of words..'),(-98, 'naturalId-99', -98,'10-12','WORD MEANINGS:..)');
 INSERT INTO common_core_standard VALUES (-99, 'naturalId-99', 1,'ccommon core -99'),(-98, 'naturalId-98', 2, 'common core -98');
 INSERT INTO item_difficulty_cuts  VALUES (-99, 1, 2, -99,-1.93882,-0.43906),(-98 ,1, 2, -98,-1.51022,0.14288);
-INSERT INTO language VALUES (-99,'lan'),(-98,'gua');
-INSERT INTO accommodation_translation VALUES (-99, -99,'Hola'),(-98, -98,'Hello');
+INSERT INTO accommodation_translation(accommodation_id, language_code, label) VALUES (-99, 'lan', 'Hola'),(-98, 'gua','Hello');
