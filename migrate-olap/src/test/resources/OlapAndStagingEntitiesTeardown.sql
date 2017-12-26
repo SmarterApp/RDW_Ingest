fe999a75032346158d5f3dc54e25236d65d36f3d
Alla Gorina <agorina@fairwaytech.com>
2017-12-26 15:49:19 -0800

    DWR-1215 Olap migrate loads the latest exam per student/asmt/year combination.

--- migrate-olap/src/test/resources/OlapAndStagingEntitiesTeardown.sql
+++ migrate-olap/src/test/resources/OlapAndStagingEntitiesTeardown.sql
@@ -1,7 +1,7 @@
 -- CLEAN UP staging
 -- ------------------------------------------  Exams ---------------------------------------------------------------------------------------------
-DELETE FROM staging_exam_claim_score where exam_id in  (-88, -87, -86, -85, -84);
-DELETE FROM staging_exam where id in (-88, -87, -86, -85, -84);
+DELETE FROM staging_exam_claim_score where exam_id in  (-88, -87, -86, -85, -84, -83);
+DELETE FROM staging_exam where id in (-88, -87, -86, -85, -84, -83, -14, -13, -17);
 
 -- ------------------------------------------ School/Districts --------------------------------------------------------------------------------------------------
 DELETE FROM staging_school where id < 0;
@@ -12,14 +12,14 @@ DELETE FROM staging_asmt where id in (-11, -99, -98);
 
 -- ------------------------------------------ Student and Groups  ------------------------------------------------------------------------------------------------
 DELETE FROM staging_student_ethnicity where student_id in ( -11, -33, -89, -88, -87, -86);
-DELETE FROM staging_student where id in ( -11, -33, -89, -88, -87, -86);
+DELETE FROM staging_student where id in ( -11, -33, -89, -88, -87, -86, -18);
 
 -- CLEAN UP reporting
-DELETE FROM fact_student_exam where id in (-88, -87, -86, -85, -84, -100);
+DELETE FROM fact_student_exam where id in (-88, -87, -86, -85, -84, -83, -100, -82);
 
 -- ------------------------------------------ Student   ------------------------------------------------------------------------------------------------
 DELETE FROM student_ethnicity where student_id in ( -11, -33, -89, -88, -87, -86);
-DELETE FROM student where id in ( -11, -33, -89, -88, -87, -86);
+DELETE FROM student where id in ( -11, -33, -89, -88, -87, -86, -18);
 
 -- ------------------------------------------ School/Districts --------------------------------------------------------------------------------------------------
 DELETE FROM school where id in (-99, -98, -1);
--- migrate-olap/src/test/resources/WarehouseEntitiesSetup.sql
+++ migrate-olap/src/test/resources/WarehouseEntitiesSetup.sql
@@ -17,12 +17,12 @@ INSERT INTO school (id, district_id, district_group_id, school_group_id, name, n
   (-99, -99, NULL, NULL, 'Sample School -99', 'natural_id-99', 1, -1, -1, '2017-07-18 20:14:34.966000', '2017-07-18 20:14:34.000000'),
   (-98, -98, -98, -98, 'Sample School -98', 'natural_id-98', 0, -2, -2, '2017-07-18 20:13:34.000000', '2017-07-18 20:13:34.000000');
 
-INSERT INTO asmt (id, natural_id, grade_id, type_id, subject_id, school_year, name, label, version, deleted, import_id, update_import_id, created, updated) VALUES
-  (-11, '(SBAC)SBAC-ICA-ASMT TEST-11', -98, 1, 1, 1999, 'SBAC-ICA-FIXED-G4M-OA-MATH-4', 'test', '9835', 0, -5000, -5000, '2017-05-18 19:05:33.967000', '2017-05-18 20:06:34.966000'),
-  (-99, '(SBAC)SBAC-ICA-ASMT TEST', -99, 3, 1, 1999, 'SBAC-ICA-FIXED-G4M-OA-MATH-4', 'ICA', '9835', 1, -5000, -20, '2017-07-18 19:05:34.966000', '2017-07-18 19:05:34.966000'),
-  (-98, 'SBAC)SBAC-ICA-ASMT TEST-5', -98, 1, 2, 1999, 'SBAC-ICA-FIXED-G5E-COMBINED-2017', 'ICA', '9831', 0, -20, -20, '2017-07-18 19:05:34.966000', '2017-07-18 19:05:34.966000'),
-  (-97, 'SBAC)SBAC-IAB-ASMT TEST', -98, 2, 2, 1999, 'SBAC-IAB-FIXED-G5E-COMBINED-2017', 'Grade 5 ELA', '9831', 0, -20, -20, '2017-07-18 19:05:34.966000', '2017-07-18 19:05:34.966000');
-
+INSERT INTO asmt
+  (id, type_id, natural_id,                    grade_id, subject_id, school_year, name, label, version, deleted, import_id, update_import_id, created, updated) VALUES
+  (-11, 1,     'ica-1999-grade-98-subject-1',       -98,   1,        1999,        'ica-1999-grade-98-subject-1',        'test',       '9835', 0,   -5000, -5000, '2017-05-18 19:05:33.967000', '2017-05-18 20:06:34.966000'),
+  (-99, 3,     'summative-1999-grade-99-subject-1', -99,   1,        1999,        'summative-1999-grade-99-subject-1',  'summative',  '9835', 1,   -5000,   -20, '2017-07-18 19:05:34.966000', '2017-07-18 19:05:34.966000'),
+  (-98, 1,     'ica-1999-grade-98-subject-2',       -98,   2,        1999,        'ica-1999-grade-98-subject-2',        'ica',        '9831', 0,     -20,   -20, '2017-07-18 19:05:34.966000', '2017-07-18 19:05:34.966000'),
+  (-97, 2,     'iab-1999-grade-98-subject-2',       -98,   2,        1999,        'iab-1999-grade-98-subject-2',        'iab',        '9831', 0,     -20,   -20, '2017-07-18 19:05:34.966000', '2017-07-18 19:05:34.966000');
 
 INSERT INTO student (id, ssid, last_or_surname, first_name, middle_name, gender_id, first_entry_into_us_school_at, lep_entry_at,
                                     lep_exit_at, birthday, import_id, update_import_id, deleted, created, updated) VALUES
@@ -40,20 +40,20 @@ INSERT INTO student_ethnicity(student_id, ethnicity_id) values
   (-86,  -98),
   (-86,  -99);
 
-INSERT INTO  exam ( id, type_id, school_year, asmt_id, asmt_version, opportunity, completeness_id,
-                    administration_condition_id, session_id, performance_level, scale_score, scale_score_std_err, completed_at,
+INSERT INTO  exam ( id, type_id, school_year, asmt_id, student_id, completed_at,
+                    asmt_version, opportunity, completeness_id,
+                    administration_condition_id, session_id, performance_level, scale_score, scale_score_std_err,
                     import_id, update_import_id, deleted, created, updated,
-                    grade_id, student_id, school_id, iep, lep, section504, economic_disadvantage,
+                    grade_id, school_id, iep, lep, section504, economic_disadvantage,
                     migrant_status, eng_prof_lvl, t3_program_type, language_code, prim_disability_type) VALUES
-  (-88, 3, 1999, -99,  null, 1, 1, 1, 'session', 1, 2145, 20.17, '2016-08-14 19:05:33.967000', -5000, -88, 1, '2017-05-18 19:05:33.967000', '2017-07-18 19:06:07.966000', -98, -89, -1, 1, 1, 0, 0, 1, 'eng_prof_lvl', 't3_program_type', 'eng', null),
-  (-87, 1, 1999, -11,  null, 1, 1, 1, 'session', 1, 2106, 21.17, '2016-08-14 19:06:07.966000', -88, -88, 0, '2017-07-18 19:06:07.966000', '2017-07-18 19:06:07.966000', -98, -11, -1, 1, 1, 0, 0, 1, 'eng_prof_lvl', 't3_program_type', 'eng', null),
-  (-86, 2, 1999, -97,  null, 1, 1, 1, 'session', 1, 2135, 30.17, '2016-08-14 19:06:07.966000', -88, -88, 0, '2017-07-18 19:06:07.966000', '2017-07-18 19:06:07.966000', -98, -11, -1, 1, 1, 0, 0, 1, 'eng_prof_lvl', 't3_program_type', 'eng', null),
-  (-85, 1, 1999, -11,  null, 1, 1, 1, 'session', 1, 2125, 40.17, '2016-08-14 19:06:07.966000', -88, -88, 0, '2017-07-18 19:06:07.966000', '2017-07-18 19:06:07.966000', -98, -11, -1, 1, 1, 0, 0, 1, 'eng_prof_lvl', 't3_program_type', 'eng', null),
+  (-88, 3, 1999, -99, -89, '2016-08-14 19:05:33.967000', null, 1, 1, 1, 'session', 1, 2145, 20.17, -5000, -88, 1, '2017-05-18 19:05:33.967000', '2017-07-18 19:06:07.966000', -98, -1, 1, 1, 0, 0, 1, 'eng_prof_lvl', 't3_program_type', 'eng', null),
+  (-87, 1, 1998, -11, -11, '2016-08-14 19:06:07.966000', null, 1, 1, 1, 'session', 1, 2106, 21.17,  -88, -88, 0,   '2017-07-18 19:06:07.966000', '2017-07-18 19:06:07.966000', -98, -1, 1, 1, 0, 0, 1, 'eng_prof_lvl', 't3_program_type', 'eng', null),
+  (-86, 2, 1999, -97, -11, '2016-08-14 19:06:07.966000', null, 1, 1, 1, 'session', 1, 2135, 30.17,  -88, -88, 0, '2017-07-18 19:06:07.966000', '2017-07-18 19:06:07.966000', -98, -1, 1, 1, 0, 0, 1, 'eng_prof_lvl', 't3_program_type', 'eng', null),
+  (-85, 1, 1999, -11, -11, '2016-08-14 19:06:07.966000', null, 1, 1, 1, 'session', 1, 2125, 40.17,  -88, -88, 0, '2017-07-18 19:06:07.966000', '2017-07-18 19:06:07.966000', -98, -1, 1, 1, 0, 0, 1, 'eng_prof_lvl', 't3_program_type', 'eng', null),
 
-  (-15, 3, 1999, -99,  null, 1, 1, 1, 'session', 1, null, 40.17, '2016-08-14 19:06:07.966000', -88, -88, 0, '2017-07-18 19:06:07.966000', '2017-07-18 19:06:07.966000', -98, -11, -1, 1, 1, 0, 0, 1, 'eng_prof_lvl', 't3_program_type', 'eng', null),
-  (-16, 1, 1999, -98,  null, 1, 1, 1, 'session', 1, 2125, null,  '2016-08-14 19:06:07.966000', -88, -88, 0, '2017-07-18 19:06:07.966000', '2017-07-18 19:06:07.966000', -98, -11, -1, 1, 1, 0, 0, 1, 'eng_prof_lvl', 't3_program_type', 'eng', null),
-  (-17, 1, 1999, -98,  null, 1, 1, 1, 'session', null, 2125, 40.17, '2016-08-14 19:06:07.966000', -88, -88, 0, '2017-07-18 19:06:07.966000', '2017-07-18 19:06:07.966000', -98, -11, -1, 1, 1, 0, 0, 1, 'eng_prof_lvl', 't3_program_type', 'eng', null);
-
+  (-15, 3, 1999, -99, -11, '2016-08-14 19:06:07.966000', null, 1, 1, 1, 'session', 1, null, 40.17,  -88, -88, 0, '2017-07-18 19:06:07.966000', '2017-07-18 19:06:07.966000', -98, -1, 1, 1, 0, 0, 1, 'eng_prof_lvl', 't3_program_type', 'eng', null),
+  (-16, 1, 1999, -98, -11, '2016-08-14 19:06:07.966000', null, 1, 1, 1, 'session', 1, 2125, null, -88, -88, 0, '2017-07-18 19:06:07.966000', '2017-07-18 19:06:07.966000', -98, -1, 1, 1, 0, 0, 1, 'eng_prof_lvl', 't3_program_type', 'eng', null),
+  (-17, 1, 1999, -98, -11, '2016-08-14 19:06:07.966000', null, 1, 1, 1, 'session', null, 2125, 40.17,  -88, -88, 0, '2017-07-18 19:06:07.966000', '2017-07-18 19:06:07.966000', -98, -1, 1, 1, 0, 0, 1, 'eng_prof_lvl', 't3_program_type', 'eng', null);
 
 INSERT INTO exam_claim_score (id, exam_id, subject_claim_score_id, scale_score, scale_score_std_err, category) VALUES
    (-1, -88, 1, 2014, 0.19, 1),
