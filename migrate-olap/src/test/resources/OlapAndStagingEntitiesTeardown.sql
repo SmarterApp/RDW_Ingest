f99d7ae58bf9a1350ec39aae8e2ca240e65a08b5
Alla Gorina <agorina@fairwaytech.com>
2017-12-27 10:17:11 -0800

    DWR-1215 Migrate district/school groups

--- migrate-olap/src/test/resources/OlapAndStagingEntitiesTeardown.sql
+++ migrate-olap/src/test/resources/OlapAndStagingEntitiesTeardown.sql
@@ -6,6 +6,8 @@ DELETE FROM staging_exam where id in (-88, -87, -86, -85, -84, -83, -14, -13, -1
 -- ------------------------------------------ School/Districts --------------------------------------------------------------------------------------------------
 DELETE FROM staging_school where id < 0;
 DELETE FROM staging_district where id in (-99, -98, -1);
+DELETE FROM staging_district_group where id in (-98);
+DELETE FROM staging_school_group where id in (-98, -1);
 
 -- ------------------------------------------ Asmt ---------------------------------------------------------------------------------------------------------
 DELETE FROM staging_asmt where id in (-11, -99, -98, -97);
@@ -24,6 +26,8 @@ DELETE FROM student where id in ( -11, -33, -89, -88, -87, -86, -18);
 -- ------------------------------------------ School/Districts --------------------------------------------------------------------------------------------------
 DELETE FROM school where id in (-99, -98, -1);
 DELETE FROM district where id in (-99, -98, -1);
+DELETE FROM district_group where id in (-98);
+DELETE FROM school_group where id in (-98, -1);
 
 -- ------------------------------------------ Asmt ---------------------------------------------------------------------------------------------------------
 DELETE FROM asmt where id in ( -11, -99, -98, -19, -97);
