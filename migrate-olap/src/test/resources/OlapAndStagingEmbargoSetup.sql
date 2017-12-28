c06fae54ff8af96ec9f7cc6dc2f9ca18680eabc4
Alla Gorina <agorina@fairwaytech.com>
2017-12-28 11:47:05 -0800

    DWR-1215 Fixed test data

--- migrate-olap/src/test/resources/OlapAndStagingEmbargoSetup.sql
+++ migrate-olap/src/test/resources/OlapAndStagingEmbargoSetup.sql
@@ -3,12 +3,12 @@ INSERT INTO district (id, name, natural_id, migrate_id) VALUES
   (-99, 'Sample District -99', 'natural_id-99', -10),
   (-98, 'Sample District -98', 'natural_id-98', -10);
 
-INSERT INTO school (id, district_id, name, natural_id, embargo_enabled, migrate_id) VALUES
-  (-99, -99, 'Sample School -99', 'natural_id-99', 0, -10),
-  (-98, -98, 'Sample School -98', 'natural_id-98', 0, -10),
-  (-97, -98, 'Sample School -97', 'natural_id-97', 0, -10);
+INSERT INTO school (id, district_id, name, natural_id, embargo_enabled, migrate_id, update_import_id) VALUES
+  (-99, -99, 'Sample School -99', 'natural_id-99', 0, -10, -1),
+  (-98, -98, 'Sample School -98', 'natural_id-98', 0, -10, -1),
+  (-97, -98, 'Sample School -97', 'natural_id-97', 0, -10, -1);
 
 -- and the district embargo staging ...
-INSERT INTO staging_district_embargo (district_id, individual, aggregate, migrate_id) VALUES
-  (-99, 0, 1, -99),
-  (-98, 1, 1, -99);
+INSERT INTO staging_district_embargo (district_id, aggregate, migrate_id) VALUES
+  (-99, 0, -99),
+  (-98, 1, -99);
