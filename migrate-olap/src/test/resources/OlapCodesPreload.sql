599de7989bac9503595edae9e7099b51f468df51
Alla Gorina <agorina@fairwaytech.com>
2017-12-29 12:36:58 -0800

    Adjusted to work with the cleaned up schema.

--- migrate-olap/src/test/resources/OlapCodesPreload.sql
+++ migrate-olap/src/test/resources/OlapCodesPreload.sql
@@ -1,5 +1,5 @@
 INSERT INTO school_year(year) VALUES (1999);
-INSERT INTO grade (id, code, name) VALUES (-99,'AA','before test');
+INSERT INTO grade (id, code) VALUES (-99, 'AA');
 INSERT INTO completeness (id, code) VALUES (-99, 'before');
 INSERT INTO administration_condition VALUES (-99,'before test');
 INSERT INTO ethnicity VALUES (-99,'before test');
