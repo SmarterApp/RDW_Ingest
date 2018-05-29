6c807a547145c7e4d0998ba8af686b66256f2da8
Alla Gorina <agorina@fairwaytech.com>
2018-05-29 14:50:39 -0700

    Clean up test SQL to respect table columns.

--- migrate-olap/src/test/resources/WarehouseCodesForDeleteSetup.sql
+++ migrate-olap/src/test/resources/WarehouseCodesForDeleteSetup.sql
@@ -1,7 +1,7 @@
 INSERT INTO school_year(year) VALUES (1998);
-INSERT INTO grade (id, code, name, sequence) VALUES (108, '20', '8th-grade-test', 8);
-INSERT INTO elas (id, code) VALUES (-98, 'E98');
-INSERT INTO completeness (id, code) VALUES (-98, 'test55');
-INSERT INTO administration_condition VALUES (-98, 'NS-test'), (-97, 'SD-test'), (-96, 'Valid-test');
-INSERT INTO ethnicity VALUES (-98, 'Asian-test');
-INSERT INTO gender VALUES (-98, 'Male-test');
+INSERT INTO grade(id, code, name, sequence) VALUES (108, '20', '8th-grade-test', 8);
+INSERT INTO elas(id, code) VALUES (-98, 'E98');
+INSERT INTO completeness(id, code) VALUES (-98, 'test55');
+INSERT INTO administration_condition(id, code) VALUES (-98, 'NS-test'), (-97, 'SD-test'), (-96, 'Valid-test');
+INSERT INTO ethnicity(id, code) VALUES (-98, 'Asian-test');
+INSERT INTO gender(id, code) VALUES (-98, 'Male-test');
