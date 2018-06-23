d099fbcd720081879711495bdaf949a6d7695ee9
Alla Gorina <agorina@fairwaytech.com>
2018-06-22 17:21:08 -0700

    Code review feedback; updated schema version; fixed broken test data.

--- group-processor/src/test/resources/WarehouseCodesSetup.sql
+++ group-processor/src/test/resources/WarehouseCodesSetup.sql
@@ -9,8 +9,8 @@ INSERT INTO administration_condition VALUES (-99,'IN-test'),(-98,'NS-test'),(-97
 INSERT INTO ethnicity VALUES (-99,'AmericanIndianOrAlaskaNative-test'),(-98,'Asian-test');
 INSERT INTO gender VALUES (-99,'Female-test'),(-98,'Male-test');
 INSERT INTO accommodation (id, code) VALUES(-99, 'code1-test'),( -98 ,'code2-test');
-INSERT INTO claim VALUES (-99 ,2,'1-IT','Read Analytically: Informational Text','Read Analytically: Informational Text ...'),( -98,2,'2-W','Write Effectively','Write Effectively - ..');
-INSERT INTO depth_of_knowledge  VALUES (-99,1,1,'Recall and Reproduction','something'),(-98,2,1,'Basic Skills and Concepts','anything');
+INSERT INTO claim VALUES (-99 ,2,'t1-IT','Read Analytically: Informational Text','Read Analytically: Informational Text ...'),( -98,2,'t2-W','Write Effectively','Write Effectively - ..');
+INSERT INTO depth_of_knowledge  VALUES (-99,-1,1,'Recall and Reproduction','something'),(-98,-2,1,'Basic Skills and Concepts','anything');
 INSERT INTO math_practice  VALUES (-99,'Make sense of problems and persevere in solving them', '-9'),(-98,'Reason abstractly and quantitatively', '-8');
 INSERT INTO item_trait_score VALUES (-99,'Conventions-test'),(-98,'Evidence/Elaboration-test');
 INSERT INTO target VALUES (-99, 'natural-99', -99,'10-1','WORD MEANINGS: Determine intended meanings of words..'),(-98, 'naturalId-99', -98,'10-12','WORD MEANINGS:..)');
