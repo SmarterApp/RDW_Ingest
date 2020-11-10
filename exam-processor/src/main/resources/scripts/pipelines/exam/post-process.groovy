3ed7f18caf6c68cf3e72f8389627f2d5e88eff48
Junyu <chenjunyu1005@gmail.com>
2020-11-10 17:40:13 -0500

    pin release common version

--- exam-processor/src/main/resources/scripts/pipelines/exam/post-process.groovy
+++ exam-processor/src/main/resources/scripts/pipelines/exam/post-process.groovy
@@ -1,5 +1,3 @@
-import org.opentestsystem.rdw.utils.DataElementErrorCollector
-
 //
 // System validation script for ingested TRTs.
 //
@@ -40,9 +38,8 @@ if (!assessment.gradeCode.equalsIgnoreCase(formattedGrade))
 // Use existing processing services for additional validation
 //
 // added to combine exam and student validation error
-def errorCollector = new DataElementErrorCollector();
-def exam = studentExamProcessor.parseExam(report, schoolId, assessment,errorCollector)
-examineeProcessor.parseStudent(report.examinee, exam, schoolId,errorCollector)
+def exam = studentExamProcessor.parseExam(report, schoolId, assessment,getDataElementErrorCollector())
+examineeProcessor.parseStudent(report.examinee, exam, schoolId,getDataElementErrorCollector())
 
 // Check if any errors have been added to the error collector. If so throw a combined error.
 // Otherwise returns true (valid). (This should be the last line of any system validation script.)
